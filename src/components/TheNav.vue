8ac9c5c61e5d2223c1e4977857efc2e4feeeffd8
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-09-10 18:48:51 +0300

    refactor: change app structure to components (#3)

---
+++ src/components/TheNav.vue
@@ -0,0 +1,20 @@
+<script setup>
+import NavItem from './NavItem.vue'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItem>
+    </ul>
+  </nav>
+</template>
