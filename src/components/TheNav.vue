51c3848cd81db10f9f0abdb9ecd966f19a19ed20
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-11 12:16:19 +0300

    feat: adding menu highlighting

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,18 +1,40 @@
 <script setup>
 import NavItem from './NavItem.vue'
+import { ref } from 'vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  } else {
+    window.location.hash = PAGE_TIMELINE
+  }
+
+  return PAGE_TIMELINE
+}
+
+const currentPage = ref(normalizePageHash())
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
