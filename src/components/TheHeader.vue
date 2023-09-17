cdc764fde1e9c65eda391b6467f09565bf8f282b
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-17 15:29:09 +0300

    feat: add functinal to links in header; refactor: move some functioms to separate file

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,13 @@
 <script setup>
-import TheLogo from './TheLogo.vue'
-import TheHeaderProgress from './TheHeaderProgress.vue'
+import TheLogo from './TheLogo.vue';
+import TheHeaderProgress from './TheHeaderProgress.vue';
+
+const emit = defineEmits(['goToTimeline', 'goToProgress']);
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b p-3 bg-slate-300">
-    <TheLogo />
-    <TheHeaderProgress />
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b p-3 bg-gray-200">
+    <TheLogo @click="emit('goToTimeline')" />
+    <TheHeaderProgress @click="emit('goToProgress')" />
   </header>
 </template>
