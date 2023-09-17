03a6ca8d933f5b1d57156a612154180fe0305c94
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-09-17 15:38:03 +0300

    feat: add functinal to links in header; refactor: move some functioms to separate file (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-import TheNav from './components/TheNav.vue'
-import TheHeader from './components/TheHeader.vue'
-import TheTimeline from './pages/TheTimeline.vue'
-import TheActivities from './pages/TheActivities.vue'
-import TheProgress from './pages/TheProgress.vue'
-import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants'
+import { ref } from 'vue';
+import TheNav from './components/TheNav.vue';
+import TheHeader from './components/TheHeader.vue';
+import TheTimeline from './pages/TheTimeline.vue';
+import TheActivities from './pages/TheActivities.vue';
+import TheProgress from './pages/TheProgress.vue';
+import { normalizePageHash } from './utils';
+import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants';
 
-const currentPage = ref(normalizePageHash())
+const currentPage = ref(normalizePageHash());
 
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-
-  if ([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
-    return hash
-  } else {
-    window.location.hash = PAGE_TIMELINE
-  }
-
-  return PAGE_TIMELINE
-}
+const goTo = (page) => {
+  currentPage.value = page;
+};
 </script>
 
 <template>
-  <TheHeader />
+  <TheHeader @go-to-timeline="goTo(PAGE_TIMELINE)" @go-to-progress="goTo(PAGE_PROGRESS)" />
   <main class="flex flex-grow flex-col">
     <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
     <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
-  <TheNav :current-page="currentPage" v-on:navigate="currentPage = $event" />
+  <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
