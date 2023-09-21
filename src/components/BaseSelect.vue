33f4c2a6fc7a9d44bb2d9f4a44f9e4a9c277d252
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-21 14:06:47 +0300

    feat: add validation functions and saving dropdown list

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,27 +1,39 @@
 <script setup>
 import BaseButton from './BaseButton.vue';
-import { validateSelectOptions } from '../validator';
+import { computed } from 'vue';
+import { validateSelectOptions, isUndefinedOrNull, isNumberOrNull } from '../validator';
 import { XMarkIcon } from '@heroicons/vue/24/outline';
 
-defineProps({
+const props = defineProps({
   selected: Number,
   options: {
     required: true,
     type: Array,
-    validator: validateSelectOptions,
+    validator: validateSelectOptions
   },
   placeholder: {
     required: true,
     type: String
   }
 });
+
+const emit = defineEmits({
+  select: isNumberOrNull
+});
+
+const isNotSelected = computed(() => isUndefinedOrNull(props.selected));
 </script>
 
 <template>
   <div class="flex gap-2">
-    <BaseButton> <XMarkIcon class="h-8" /></BaseButton>
-    <select class="w-full truncate rounded bg-gray-100 py-1 px2 text-2xl">
-      <option selected disabled value="">{{ placeholder }}</option>
+    <BaseButton @click="emit('select', null)"> <XMarkIcon class="h-8" /></BaseButton>
+    <select
+      class="w-full truncate rounded bg-gray-100 py-1 px2 text-2xl"
+      @change="emit('select', +$event.target.value)"
+    >
+      <option :selected="isNotSelected" disabled value="">
+        {{ placeholder }}
+      </option>
       <option
         v-for="{ value, label } in options"
         :key="value"
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,4 @@
-import { MIDNIGHT_HOUR, NAV_ITEMS } from './constants';
-import { HOURS_IN_DAY } from './constants';
+import { MIDNIGHT_HOUR, NAV_ITEMS, HOURS_IN_DAY } from './constants';
 
 export const isPageValid = (page) => {
   return Object.keys(NAV_ITEMS).includes(page);
@@ -10,13 +9,49 @@ export const validateTimelineItems = (timelimeItems) => {
 };
 
 export const isTimelineItemValid = ({ hour }) => {
-  return typeof hour === 'number' && hour >= MIDNIGHT_HOUR && hour < HOURS_IN_DAY;
+  return isHourValid(hour);
 };
 
 export const validateSelectOptions = (options) => {
   return options.every(isSelectOptionValid);
 };
 
+export const isUndefinedOrNull = (value) => {
+  return isNull(value) || isUndefined(value);
+};
+
+export const isNumberOrNull = (value) => {
+  return isNumber(value) || isNull(value);
+};
+
+// Проверка типов
+const isNull = (value) => {
+  return value === null;
+};
+
+const isUndefined = (value) => {
+  return value === undefined;
+};
+
+const isNumber = (value) => {
+  return typeof value === 'number';
+};
+
+const isString = (value) => {
+  return typeof value === 'string';
+};
+
+// Проверка временного интервала
+const isBetween = (value, start, end) => {
+  return value >= start && value < end;
+};
+
+// Проверка валидности опции выпадающего списка
 const isSelectOptionValid = ({ value, label }) => {
-  return typeof value === 'number' && typeof label === 'string';
+  return isNumber(value) && isString(label);
+};
+
+// Проверка валидности часа
+const isHourValid = (hour) => {
+  return isNumber(hour) && isBetween(hour, MIDNIGHT_HOUR, HOURS_IN_DAY);
 };
